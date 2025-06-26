daa7a5bcce80bf815d9dd14bf06260fa297d69bd
AhmedShehata02 <a.shehata.code1@gmail.com>
2025-06-27 01:44:04 +0300

    verify all apis response as ApiResponse

--- src/app/core/services/send-email.service.ts
+++ src/app/core/services/send-email.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { EmailDto } from '../interface/email.interfaces';
+import { ApiResponse } from '../interface/api-response.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class SendEmailService {
 
   constructor(private http: HttpClient) {}
 
-  sendEmail(email: EmailDto): Observable<any> {
-    return this.http.post(`${this.apiUrl}/send`, email);
+  sendEmail(email: EmailDto): Observable<ApiResponse<string>> {
+    return this.http.post<ApiResponse<string>>(`${this.apiUrl}/send`, email);
   }
 }
